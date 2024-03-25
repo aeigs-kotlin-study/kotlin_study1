ade21c624ef6470def529363d5827ac223c732a6
김태우 <sssunggure@naver.com>
2024-03-25 16:23:31 +0900

    [ADD] a를 프린트 하는 기능 추가

--- src/Main.kt
+++ src/Main.kt
@@ -1,18 +1,6 @@
 fun main() {
     //val = 변수
-    var name1 = "variable Kotlin"
-    println("Hello, " + name1 + "!")
-    name1 = "change Kotlin"
-    println("Hello, " + name1 + "!")
+    var a = "1234"
+    println(a)
 
-
-    //val = 상수
-    val name = "Kotlin"
-    println("Hello, " + name + "!")
-
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
 }
